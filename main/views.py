99d001d41f2c799ba04bcaab59b81bde041019db
Marco <ymarcogracia@gmail.com>
2024-11-13 14:14:13 +0700

    add tutorial 8

--- main/views.py
+++ main/views.py
@@ -1,8 +1,9 @@
+import json
 from django.shortcuts import render, redirect
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.core import serializers
 
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
@@ -58,6 +59,25 @@ def add_mood_entry_ajax(request):
     new_mood.save()
 
     return HttpResponse(b"CREATED", status=201)
+
+@csrf_exempt
+def create_mood_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_mood = MoodEntry.objects.create(
+            user=request.user,
+            mood=data["mood"],
+            mood_intensity=int(data["mood_intensity"]),
+            feelings=data["feelings"]
+        )
+
+        new_mood.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+    
 
 def edit_mood(request, id):
     # Get mood entry berdasarkan id
